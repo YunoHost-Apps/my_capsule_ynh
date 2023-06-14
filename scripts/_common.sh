4b02b05a781b1dc8e1e3249ce303de95f6fb2e53
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-06-14 18:06:15 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,8 +5,8 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies=""
-app_dependencies="gemserv"
+#REMOVEME? pkg_dependencies=""
+#REMOVEME? app_dependencies="gemserv"
 
 #=================================================
 # PERSONAL HELPERS
