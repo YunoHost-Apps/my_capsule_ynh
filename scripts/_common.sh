3d52b5002bcfa8ffe83c2234553269ddd6c0d43f
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-08-15 10:14:56 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,8 +4,10 @@
 # COMMON VARIABLES
 #=================================================
 
+YNH_PHP_VERSION="7.4"
+
 # dependencies used by the app
-pkg_dependencies="php7.4-fpm php7.4-mbstring"
+pkg_dependencies="php${YNH_PHP_VERSION}-fpm php${YNH_PHP_VERSION}-mbstring"
 app_dependencies="gemserv"
 
 #=================================================
