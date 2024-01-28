4c09635cf5b950c34d0a5ab6851dd53c308fa8c2
Félix Piédallu <felix@piedallu.me>
2024-01-28 14:40:00 +0100

    Finish manifestv2 ; try to fix ci

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,6 +8,16 @@
 # PERSONAL HELPERS
 #=================================================
 
+_ynh_add_gemserv_config() {
+    ynh_add_config --template="server.toml" --destination="/etc/gemserv/config.d/$domain.toml"
+    ynh_systemd_action --service_name=gemserv --action=reload
+}
+
+_ynh_remove_gemserv_config() {
+    ynh_secure_remove --file="/etc/gemserv/config.d/$domain.toml"
+    ynh_systemd_action --service_name=gemserv --action=reload
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
